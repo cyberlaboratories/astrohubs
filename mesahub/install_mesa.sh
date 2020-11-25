86c5c129bf0bcd368c1c0a451c90ea9e8d78ab8e
fherwig <fherwig@uvic.ca>
2020-11-25 05:22:27 +0000

    add version 5329 and fix install of 7624

--- mesahub/install_mesa.sh
+++ mesahub/install_mesa.sh
@@ -9,10 +9,11 @@
 #
 # define some things
 mesa_user_dir=/user/mesa
+#mesa_version=5329 # --> Pavel (90Msun and supermassive stars)
 #mesa_version=7624 # --> Pavel (HB stars, RAWDs), Rob Farmer
 #mesa_version=7184 # --> Austin (massive stars)
 #mesa_version=8118 # --> Ondrea (Pop III)
-#mesa_version=8845 # --> Jacqueline (rotating models, massive stars in the future)
+#mesa_version=8845 # --> Jacqueline (rotating models, massive stars)
 #mesa_version=9331 # --> tested mesa_h5 output with
 mesa_version=10398 
 #mesa_version=12115 # --> Brad RCB stars
@@ -21,7 +22,7 @@ mesa_version=10398
 # probably nothing needs to be changed below here
 mesa_source_dir=$mesa_user_dir/mesa_$mesa_version
 [ ! -d $mesa_user_dir ] && mkdir $mesa_user_dir
-if [ ! -d $mesa_source_dir ] 
+if [ ! -d $mesa_source_dir ]  
 then
     echo Installing into $mesa_user_dir
     if [ $mesa_version -lt 12115 ]
@@ -30,15 +31,36 @@ then
     else
         svn co -r $mesa_version https://subversion.assembla.com/svn/mesa^mesa/trunk $mesa_source_dir
     fi
+    if [ -h $MESA_DIR ]
+    then
+	echo $MESA_DIR points to existing version. Remove link and set to new version $mesa_version.
+	rm $MESA_DIR
+    else
+	echo "Make link of $MESA_DIR to version $mesa_version in /user/mesa"
+    fi
     ln -s $mesa_source_dir $MESA_DIR  # $MESA_DIR is defined in .bash_aliases
     cd $mesa_source_dir/utils
     sed -i s/"USE_PGSTAR = YES"/"USE_PGSTAR = NO"/g makefile_header
     sed -i /"LOAD_PGPLOT ="/c\ "LOAD_PGPLOT =" makefile_header
-    if [[ $mesa_version =~ ^7 ]]
+    if [[ $mesa_version =~ ^7 ]] || [[ $mesa_version =~ ^5 ]]
     then
         sed -i '/FCbasic = -fno-range-check/s/$/ -Wno-uninitialized/' makefile_header
     fi
     cd ..
+    if [[ $mesa_version =~ ^5 ]]
+    then
+	cd star/private
+	sed -i s/"stop'fixup'"/"stop 'fixup'"/g mod_diffusion_procs.f
+	cd ../public
+	sed -i s/"call do_pgstar_plots"/"\!call do_pgstar_plots"/g star_lib.f
+	cd ../..
+    fi
+    if [[ $mesa_version =~ ^7 ]] 
+    then
+	cd star/private
+	sed -i s/"stop'fixup'"/"stop 'fixup'"/g diffusion_procs.f90
+	cd ../..
+    fi
     ./clean
     ./install
     echo "MESA installation version $mesa_version complete in $mesa_source_dir"
